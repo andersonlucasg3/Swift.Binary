5872273c7b7697acf2b045855de9827ddd3748e2
Anderson Lucas C. Ramos <anderson@insanitech.com.br>
2017-04-19 00:52:49 -0300

    Added target definition for SwiftBinary;

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftBinary",
     targets: [
-		Target(name: "Swift.Binary")
+		Target(name: "SwiftBinary")
 	],
     dependencies: [
 	]
